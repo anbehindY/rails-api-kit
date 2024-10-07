faecbe592bf40818233e598efc2ca49842ec833a
anbehindY <yanminthwin@gmail.com>
2024-10-07 20:58:09 +0700

    Configure Standard Linter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,17 +7,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
-      @token = request.env['warden-jwt_auth.token']
-      headers['Authorization'] = @token
+      @token = request.env["warden-jwt_auth.token"]
+      headers["Authorization"] = @token
 
       render json: {
-        status: { code: 200, message: 'Signed up successfully.',
-                  token: @token,
-                  data: UserSerializer.new(resource).serializable_hash[:data][:attributes] }
+        status: {code: 200, message: "Signed up successfully.",
+                 token: @token,
+                 data: UserSerializer.new(resource).serializable_hash[:data][:attributes]}
       }
     else
       render json: {
-        status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
       }, status: :unprocessable_entity
     end
   end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,12 +6,12 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opt = {})
-    @token = request.env['warden-jwt_auth.token']
-    headers['Authorization'] = @token
+    @token = request.env["warden-jwt_auth.token"]
+    headers["Authorization"] = @token
 
     render json: {
       status: {
-        code: 200, message: 'Logged in successfully.',
+        code: 200, message: "Logged in successfully.",
         token: @token,
         data: {
           user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
@@ -21,17 +21,17 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
-                               Rails.application.credentials.devise_jwt_secret_key!).first
+    if request.headers["Authorization"].present?
+      jwt_payload = JWT.decode(request.headers["Authorization"].split.last,
+        Rails.application.credentials.devise_jwt_secret_key!).first
 
-      current_user = User.find(jwt_payload['sub'])
+      current_user = User.find(jwt_payload["sub"])
     end
 
     if current_user
       render json: {
         status: 200,
-        message: 'Logged out successfully.'
+        message: "Logged out successfully."
       }, status: :ok
     else
       render json: {
@@ -40,7 +40,7 @@ class Users::SessionsController < Devise::SessionsController
       }, status: :unauthorized
     end
   end
-  
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
